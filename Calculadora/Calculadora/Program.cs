6e47fc4adfe619cab8f2e225254ffd5118b697c6
Alberto <alberto.gutierrez@e-quallity.net>
2020-05-15 09:04:44 -0500

    Se añadió switch

--- Calculadora/Calculadora/Program.cs
+++ Calculadora/Calculadora/Program.cs
@@ -10,6 +10,31 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
+				Console.writeln("*** Elija una opcion ***");
+                Console.writeln("Ingrese primer numero");
+                int num1 = (int)Console.readln().trim;
+                Console.writeln("Ingrese segundo numero");
+                int num2 = (int)Console.readln().trim;                
+                Console.writeln("1) suma \n2) resta \n3)multiplicacion \n4)division ");
+                int respuesta = (int)Console.readln().trim;
+                Console.writeln("la respuesta es: ");
+
+                switch (respuesta) {
+                    case 1:
+                        Console.writeln("la suma es: "+ suma(num1, num2)); 
+                        break;
+                    case 2:
+                        Console.writeln("la resta es: " + Restar(num1, num2));                                                
+                        break;
+                    case 3:
+                        Console.writeln("la multiplicacion es: " + multiplicacion(num1, num2));                                               
+                        break;
+                    case 4:
+                        Console.writeln("la division es: " + division(num1, num2));                        
+                        break;
+                    default:
+                        Console.writeln("Selecciona una opción válida");
+                }
         }
 
         public int suma(int a, int b)
