6fa9d62754749b09228b56243f863568f6a7d3d4
Alberto <alberto.gutierrez@e-quallity.net>
2020-05-15 09:18:15 -0500

    Se terminó

--- Calculadora/Calculadora/Program.cs
+++ Calculadora/Calculadora/Program.cs
@@ -10,48 +10,53 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-				Console.writeln("*** Elija una opcion ***");
-                Console.writeln("Ingrese primer numero");
-                int num1 = (int)Console.readln().trim;
-                Console.writeln("Ingrese segundo numero");
-                int num2 = (int)Console.readln().trim;                
-                Console.writeln("1) suma \n2) resta \n3)multiplicacion \n4)division ");
-                int respuesta = (int)Console.readln().trim;
-                Console.writeln("la respuesta es: ");
-
-                switch (respuesta) {
+				Console.WriteLine("*** Elija una opcion ***");
+                Console.WriteLine("Ingrese primer numero:");
+                int num1 = int.Parse(Console.ReadLine().Trim());
+                Console.WriteLine("Ingrese segundo numero:");
+                int num2 = int.Parse(Console.ReadLine().Trim());
+                Console.WriteLine("1) suma \n2) resta \n3)multiplicacion \n4)division ");
+                int respuesta = int.Parse(Console.ReadLine().Trim());
+                 Console.WriteLine("la respuesta es: ");
+                switch (respuesta)
+                {
                     case 1:
-                        Console.writeln("la suma es: "+ suma(num1, num2)); 
+                        Console.WriteLine("la suma es: "+ suma(num1, num2));
+                        Console.ReadLine();
                         break;
                     case 2:
-                        Console.writeln("la resta es: " + Restar(num1, num2));                                                
+                        Console.WriteLine("la resta es: " + Restar(num1, num2));
+                        Console.ReadLine();
                         break;
                     case 3:
-                        Console.writeln("la multiplicacion es: " + multiplicacion(num1, num2));                                               
+                        Console.WriteLine("la multiplicacion es: " + multiplicacion(num1, num2));
+                        Console.ReadLine();
                         break;
                     case 4:
-                        Console.writeln("la division es: " + division(num1, num2));                        
-                        break;
+                        Console.WriteLine("la division es: " + division(num1, num2));
+                        Console.ReadLine();
+                    break;
                     default:
-                        Console.writeln("Selecciona una opción válida");
-                }
+                        Console.WriteLine("Selecciona una opción válida");
+                    break;
+                }            
         }
 
-        public int suma(int a, int b)
+        public static int suma(int a, int b)
         {
             return a + b;
         }
 
-        public int Restar(int A, int B)
+        public static int Restar(int A, int B)
 		{
 			return A - B;
 		}	
 		
-		public double multiplicacion(double num1, double num2)
+		public static double multiplicacion(double num1, double num2)
 		{
 			return num1 * num2;			
 		}
-		public double division (double num1, double num2)
+		public static double division (double num1, double num2)
 		{
      	 return num1 / num2;
 		}
