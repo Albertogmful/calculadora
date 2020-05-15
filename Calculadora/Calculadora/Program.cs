d221ea600d575edf47d3c26142ed7438e77ddc60
Alberto <alberto.gutierrez@e-quallity.net>
2020-05-15 08:29:12 -0500

    Se quitaron datos innecesarios

--- Calculadora/Calculadora/Program.cs
+++ Calculadora/Calculadora/Program.cs
@@ -18,13 +18,9 @@ namespace Calculadora
         {
             return a + b;
         }
-<<<<<<< Updated upstream
 		
 		public double multiplicacion(double num1, double num2){
 			return num1 * num2;			
 		}
-=======
-
->>>>>>> Stashed changes
     }
 }
